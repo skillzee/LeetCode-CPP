27e2a11a57574e4231385b1426ed1972e7c05ddd
Parth Sharma <112481963+skillzee@users.noreply.github.com>
2025-02-28 21:40:51 +0530

    Time: 4 ms (81.81%), Space: 15.7 MB (39.61%) - LeetHub

--- 0494-target-sum/0494-target-sum.cpp
+++ 0494-target-sum/0494-target-sum.cpp
@@ -1,29 +1,39 @@
 class Solution {
 public:
-    int solve(int ind, vector<int>& nums, int target, unordered_map<string, int>& memo) {
-        // Base case
-        if (ind == nums.size()) {
-            return target == 0 ? 1 : 0;
-        }
-
-        // Create a unique key for the current state (ind, target)
-        string key = to_string(ind) + "," + to_string(target);
-
-        // If this state has been solved before, return the cached result
-        if (memo.find(key) != memo.end()) {
-            return memo[key];
-        }
-
-        // Recursively explore adding and subtracting the current number
-        int plus = solve(ind + 1, nums, target - nums[ind], memo);
-        int minus = solve(ind + 1, nums, target + nums[ind], memo);
-
-        // Cache the result before returning
-        return memo[key] = plus + minus;
-    }
-
     int findTargetSumWays(vector<int>& nums, int target) {
-        unordered_map<string, int> memo;
-        return solve(0, nums, target, memo);
+        int total = accumulate(nums.begin(), nums.end(), 0);
+        // Ye similar hai partitions with given difference jo gfg pe kiya tha do subsets mein baat do aur unn dono ka minus krke difference target ke equal hona chahiye
+
+        int s1 = (target+total)/2;
+        if(total < target) return 0;
+        if((total+target)<0 || ((total+target) % 2) != 0) return 0;
+        
+        int n = nums.size();
+
+        vector<vector<int>> dp(n+1, vector<int>(s1+1));
+        for(int i = 0; i<n+1; i++){
+            for(int j = 0;j<s1+1; j++){
+                if(j == 0) dp[i][j] = 1;
+
+                if(i == 0 and j !=0 ) dp[i][j] = 0;
+            }
+        }
+
+
+        for(int i = 1; i<n+1; i++){
+            for(int j = 0; j<s1+1; j++){
+                if(nums[i-1]<=j){
+                    dp[i][j] = dp[i-1][j-nums[i-1]] + dp[i-1][j];
+                }
+                else{
+                    dp[i][j] = dp[i-1][j];
+                }
+            }
+        }
+
+        return dp[n][s1];
+
+
+
     }
 };
