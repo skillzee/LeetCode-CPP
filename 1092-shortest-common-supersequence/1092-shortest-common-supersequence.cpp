15f8b8aa1c0ba46559906a8f58aa843854479950
Parth Sharma <112481963+skillzee@users.noreply.github.com>
2025-03-25 12:25:36 +0530

    Time: 27 ms (35.78%), Space: 26.8 MB (38.21%) - LeetHub

--- 1092-shortest-common-supersequence/1092-shortest-common-supersequence.cpp
+++ 1092-shortest-common-supersequence/1092-shortest-common-supersequence.cpp
@@ -1,52 +1,52 @@
 class Solution {
 public:
-    string shortestCommonSupersequence(string str1, string str2) {
-        int n = str1.size();
-        int m = str2.size();
-        vector<vector<int>> dp(n+1, vector<int>(m+1, 0));
-
-        // This DP table is sshowing the common elements between both indices at their respective lengths.
-   
-        for(int i = 1; i<=n; i++){
-            for(int j =1; j<=m; j++){
-                if(str1[i-1] == str2[j-1]){
-                    dp[i][j] = 1+dp[i-1][j-1];
-                }else{
+    void lcs(string s1, string s2, vector<vector<int>> &dp) {
+        int n = s1.length(), m = s2.length();
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= m; j++) {
+                if (s1[i-1] == s2[j-1]) {
+                    dp[i][j] = 1 + dp[i-1][j-1];
+                } else {
                     dp[i][j] = max(dp[i-1][j], dp[i][j-1]);
                 }
             }
         }
+    }
 
-       
+    string shortestCommonSupersequence(string str1, string str2) {
+        int n = str1.length(), m = str2.length();
+        vector<vector<int>> dp(n+1, vector<int>(m+1, 0));
+        lcs(str1, str2, dp);
 
-        string ans = "";
         int i = n, j = m;
-        while(i>0 and j>0){
-            if(str1[i-1] == str2[j-1]){
-                ans+=str1[i-1];
-                i--;
-                j--;
-            }
-            else if(dp[i-1][j] > dp[i][j-1]){ // max element waale case hain ye aur iske neeche wala ye dekhne ke liye ki dp table mein max konsa tha ye dekhne ke liye
-                ans+=str1[i-1];
-                i--;
-            }
-            else{
-                ans+=str2[j-1];
-                j--;
+        string s = "";
+
+        while (i > 0 && j > 0) {
+            if (str1[i - 1] == str2[j - 1]) {
+                s.push_back(str1[i - 1]);
+                i--; j--;
+            } else {
+                if (dp[i - 1][j] > dp[i][j - 1]) {
+                    s.push_back(str1[i - 1]);
+                    i--;
+                } else {
+                    s.push_back(str2[j - 1]);
+                    j--;
+                }
             }
         }
 
-        while(i>0){
-            ans+=str1[i-1];
+        while (i > 0) {
+            s.push_back(str1[i - 1]);
             i--;
         }
-        while(j>0){
-            ans+=str2[j-1];
+
+        while (j > 0) {
+            s.push_back(str2[j - 1]);
             j--;
         }
 
-        reverse(ans.begin(), ans.end());
-        return ans;
+        reverse(s.begin(), s.end());
+        return s;
     }
 };
