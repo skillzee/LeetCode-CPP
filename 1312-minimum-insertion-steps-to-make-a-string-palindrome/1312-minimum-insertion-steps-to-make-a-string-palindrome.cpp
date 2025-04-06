22dc3201d2074e781c33a1374a5a9133129e863f
Parth Sharma <112481963+skillzee@users.noreply.github.com>
2025-04-06 11:10:43 +0530

    Time: 31 ms (65.72%), Space: 31.4 MB (72.06%) - LeetHub

--- 1312-minimum-insertion-steps-to-make-a-string-palindrome/1312-minimum-insertion-steps-to-make-a-string-palindrome.cpp
+++ 1312-minimum-insertion-steps-to-make-a-string-palindrome/1312-minimum-insertion-steps-to-make-a-string-palindrome.cpp
@@ -1,25 +1,30 @@
 class Solution {
 public:
-    int minInsertions(string s) {
-        // What I will try to do is I will find the longest palindromic subsequence and then minus it from the actual length which should give me the minimum insertions
-         string s1 = s;
-        reverse(s.begin(), s.end());
-        int n = s.length();
+    int lcs(string &s1, string &s2){
+        int n = s1.length();
         vector<vector<int>> dp(n+1, vector<int>(n+1, 0));
-        for(int i = 1; i<=n; i++){
+
+        for(int i = 1; i<=n;i++){
             for(int j = 1; j<=n; j++){
-                if(s[i-1] == s1[j-1]){
-                    dp[i][j] = 1+dp[i-1][j-1]; 
+                if(s1[i-1] == s2[j-1]){
+                    dp[i][j] = 1+dp[i-1][j-1];
                 }else{
                     dp[i][j] = max(dp[i-1][j], dp[i][j-1]);
                 }
             }
         }
-        int LPS = dp[n][n];
 
-        return s.length() - LPS;
+        return dp[n][n];
+    }
+
+    int minInsertions(string s) {
+        string s1 = s;
+        // Ismein humne kya kiya hai ki pehle LPS nikala aur fir jo humare paas singular bache honge vhi to unka pair chahiye hoga to vo hi number of insertions honge aur vo hi number of deletions bhi honge.
+        reverse(s1.begin(), s1.end());
+
+        int LPS = lcs(s, s1);
         
 
-
+        return s.length()-LPS;
     }
 };
