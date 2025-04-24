fc3c5f667c553eb696626a6ea38c66c4a364defd
Parth Sharma <112481963+skillzee@users.noreply.github.com>
2025-04-24 20:59:41 +0530

    Time: 1820 ms (5.22%), Space: 473.2 MB (13.79%) - LeetHub

--- 2799-count-complete-subarrays-in-an-array/2799-count-complete-subarrays-in-an-array.cpp
+++ 2799-count-complete-subarrays-in-an-array/2799-count-complete-subarrays-in-an-array.cpp
@@ -1,21 +1,5 @@
 class Solution {
 public:
-    int findDistincts(vector<int> &nums){
-        int d = 0;
-        unordered_map<int, int> mp;
-        for(auto &it: nums){
-            mp[it]++;
-        }
-        for(auto it: mp){
-            if(it.second>1){
-                d++;
-                it.second = 0;
-            }else{
-                d++;
-            }
-        }
-        return d;
-    }
 
     int countCompleteSubarrays(vector<int>& nums) {
         unordered_set<int> st(nums.begin(), nums.end());
