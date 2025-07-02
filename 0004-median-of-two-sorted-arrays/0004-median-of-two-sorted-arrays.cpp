1a04c07b024d82760783ba53a6096ea2e4bfad3a
Parth Sharma <112481963+skillzee@users.noreply.github.com>
2025-07-02 21:10:44 +0530

    Time: 0 ms (100%), Space: 95.1 MB (59.83%) - LeetHub

--- 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.cpp
+++ 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.cpp
@@ -1,36 +1,35 @@
 class Solution {
 public:
     double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) {
-        int m = nums1.size();
-        int n = nums2.size();
-        int total = m + n;
-        int half = total / 2;
-
-        if(m > n) return findMedianSortedArrays(nums2, nums1);
-
-        int low = 0, high = m;
-        while(low <= high){
-            int mid = low + (high - low) / 2;
-            int j = half - mid;
-
-            int l1 = (mid == 0) ? INT_MIN : nums1[mid - 1];
-            int r1 = (mid == m) ? INT_MAX : nums1[mid];
-            int l2 = (j == 0) ? INT_MIN : nums2[j - 1];
-            int r2 = (j == n) ? INT_MAX : nums2[j];
-
-            if(l1 <= r2 && l2 <= r1){
-                if(total % 2 == 0){
-                    return (max(l1, l2) + min(r1, r2)) / 2.0;
-                } else {
-                    return min(r1, r2);
-                }
-            } else if(l1 > r2){
-                high = mid - 1;
-            } else {
-                low = mid + 1;
+        int n = nums1.size();
+        int m = nums2.size();
+        if(n>m) return findMedianSortedArrays(nums2, nums1);
+        int left = (n+m+1)/2;
+        int low = 0;
+        int high = n;
+        while(low<=high){
+            int mid1 = low + (high-low)/2;
+            int mid2 = left-mid1;
+            int l1 = INT_MIN;
+            int l2 = INT_MIN;
+            int r2 = INT_MAX;
+            int r1 = INT_MAX;
+            if(mid1<n) r1 = nums1[mid1];
+            if(mid2<m) r2 = nums2[mid2];
+            if(mid1-1 >=0) l1 = nums1[mid1-1]; 
+            if(mid2-1 >=0) l2 = nums2[mid2-1]; 
+            if(l1<=r2 and l2<=r1){
+                if((m+n) %2 == 1) return max(l1,l2);
+                return double(max(l1,l2) + min(r1, r2))/2.0;
+            }else if(l1>r2){
+                high = mid1-1;
+            }else{
+                low = mid1+1;
             }
+             
         }
 
-        return 0.0;
+        return 0;
+
     }
 };
