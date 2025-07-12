496f928f3a47c147e28e7f8e71dd65b05e250cef
Parth Sharma <112481963+skillzee@users.noreply.github.com>
2025-07-12 18:55:23 +0530

    Time: 0 ms (100%), Space: 10.2 MB (78.86%) - LeetHub

--- 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.cpp
+++ 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.cpp
@@ -1,24 +1,37 @@
 class Solution {
 public:
-    string reverseWords(string s) {
-        int n = s.length();
+    string reverseWords(string s){
         reverse(s.begin(), s.end());
-        string ans = "", temp = "";
-        for (int i = 0; i < n; i++) {
-            if (s[i] != ' ') {
-                temp +=s[i];
-            }else if(!temp.empty()) {
-                reverse(temp.begin(), temp.end());
-                if (!ans.empty()) ans += " ";
-                ans += temp;
-                temp = "";
+        
+        int i =0;
+
+        // ye l aur r particular words ko reverse krenge
+        int l = 0;
+        int r = 0;
+
+
+        int n = s.length();
+        while(i<n){
+            while(i<n and s[i] != ' '){
+                s[r] = s[i];
+                i++;
+                r++;
             }
+
+            if(l<r){
+                reverse(s.begin()+l, s.begin()+r);
+
+                s[r] = ' ';
+                r++;
+                l = r;
+            }
+
+            i++; // this will alsways badhta rahega.
         }
-        if (!temp.empty()) {
-            reverse(temp.begin(), temp.end());
-            if (!ans.empty()) ans += " ";
-            ans += temp;
-        }
-        return ans;
+
+        s = s.substr(0, r-1);
+
+
+        return s;
     }
 };
