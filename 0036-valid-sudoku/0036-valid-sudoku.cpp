df450a5981ab3c3a84f542f15aa7dd09302ad382
Parth Sharma <112481963+skillzee@users.noreply.github.com>
2025-08-30 22:56:47 +0530

    Time: 11 ms (16.01%), Space: 25.8 MB (12.36%) - LeetHub

--- 0036-valid-sudoku/0036-valid-sudoku.cpp
+++ 0036-valid-sudoku/0036-valid-sudoku.cpp
@@ -1,62 +1,25 @@
 class Solution {
 public:
-    bool traverse(vector<vector<char>>& board, int sr, int sc, int er, int ec){
-        set<char> st;
-        for(int i = sr; i<=er; i++){
-            for(int j = sc; j<=ec; j++){
-                if(board[i][j] == '.') continue;
-
-                if(st.find(board[i][j])!=st.end()){
-                    return false;
-                }
-
-                st.insert(board[i][j]);
-            }
-        }
-
-        return true;
-    }
-
     bool isValidSudoku(vector<vector<char>>& board) {
         
+        unordered_set<string> st;
 
-        // Validate Rows
-        for(int row = 0; row<9; row++){
-            set<char> st;
-            for(int col = 0; col<9; col++){
-                if(board[row][col] == '.'){
-                    continue;
-                }
-                if(st.find(board[row][col]) != st.end()){
+        for(int i = 0; i<9; i++){
+            for(int j = 0; j<9; j++){
+                if(board[i][j] == '.') continue;
+                string row = string(1, board[i][j]) + "ROW" + to_string(i);
+                string col = string(1, board[i][j]) + "COL" + to_string(j);
+                string box = string(1, board[i][j]) + "BOX" + to_string(i/3) + to_string(j/3);
+
+                if(st.find(row) != st.end() || st.find(col) != st.end() || st.find(box) != st.end()){
                     return false;
                 }
-                st.insert(board[row][col]);
-            }
-        }
 
 
-        // Validate Columns
-        for(int col = 0; col<9; col++){
-            set<char> st;
-            for(int row = 0; row<9; row++){
-                if(board[row][col] == '.'){
-                    continue;
-                }
-                if(st.find(board[row][col]) != st.end()){
-                    return false;
-                }
-                st.insert(board[row][col]);
-            }
-        }
+                st.insert(row);
+                st.insert(col);
+                st.insert(box);
 
-        // Validate 3x3 Boxes
-        for(int sr = 0; sr<9; sr+=3){
-            int er = sr+2;
-            for(int sc = 0; sc<9; sc+=3){
-                int ec = sc+2;
-                if(!traverse(board, sr, sc, er, ec)){
-                    return false;
-                }
             }
         }
 
