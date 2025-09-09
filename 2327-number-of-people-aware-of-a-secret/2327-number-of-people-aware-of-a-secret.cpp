46f0fc4b648a59792e024e6c81ef6cbc8c2309ce
Parth Sharma <112481963+skillzee@users.noreply.github.com>
2025-09-09 16:47:47 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 2327-number-of-people-aware-of-a-secret/2327-number-of-people-aware-of-a-secret.cpp
@@ -0,0 +1,39 @@
+class Solution {
+public:
+    int MOD = 1e9+7;
+
+
+    int solve(int day, int delay, int forget, vector<int> &dp){
+        if(day == 1) return 1;
+
+        if(dp[day] != -1){
+            return dp[day];
+        }
+
+        int res = 0;
+        for(int prev = day-forget+1; prev<=day-delay; prev++){
+            if(prev>0){
+                res = (res+solve(prev, delay, forget, dp)) % MOD;
+            }
+        }
+
+        return dp[day]=res;
+    }
+
+
+
+    int peopleAwareOfSecret(int n, int delay, int forget) {
+        int total = 0;
+        vector<int> dp(n+1, -1);
+
+        for(int day = n-forget+1; day<=n; day++){
+            if(day>0){
+                total = (total+ solve(day, delay, forget, dp)) & MOD;
+            }
+        }
+        
+
+        return total;
+
+    }
+};
