a8acba28afdc0a32668f0e2c161b42d6b15b229f
Parth Sharma <112481963+skillzee@users.noreply.github.com>
2025-09-12 14:25:33 +0530

    Time: 0 ms (100%), Space: 9.4 MB (13.25%) - LeetHub

--- 0054-spiral-matrix/0054-spiral-matrix.cpp
+++ 0054-spiral-matrix/0054-spiral-matrix.cpp
@@ -1,14 +1,17 @@
 class Solution {
 public:
-    vector<int> solve(vector<vector<int>> matrix) {
+    vector<int> spiralOrder(vector<vector<int>>& matrix) {
         int n = matrix.size();
         int m = matrix[0].size();
 
+        int left = 0;
+        int top = 0;
+        int right = m - 1;
+        int bottom = n - 1;
+
         vector<int> ans;
 
-        int left = 0, right = m - 1, top = 0, bottom = n - 1;
-
-        while (left <= right && top <= bottom) {
+        while (left <= right and top <= bottom) {
             for (int i = left; i <= right; i++) {
                 ans.push_back(matrix[top][i]);
             }
@@ -20,6 +23,7 @@ public:
             right--;
 
             if (top <= bottom) {
+
                 for (int i = right; i >= left; i--) {
                     ans.push_back(matrix[bottom][i]);
                 }
@@ -27,6 +31,7 @@ public:
             }
 
             if (left <= right) {
+
                 for (int i = bottom; i >= top; i--) {
                     ans.push_back(matrix[i][left]);
                 }
@@ -34,11 +39,6 @@ public:
             }
         }
 
-        return ans;
-    }
-
-    vector<int> spiralOrder(vector<vector<int>>& matrix) {
-        vector<int> ans = solve(matrix);
         return ans;
     }
 };
